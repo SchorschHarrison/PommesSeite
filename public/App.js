cb5971df8cdd0c2ced957dc6e64b08b01830b269
Ann-Christin Gehring <ann-christin.gehring@gmx.de>
2018-10-26 17:20:08 +0200

    Anpassungen für Zusammenlegung von verschiedenen Projekten
    altes index.html und app.js verglichen und übertragen,
    Array in app.js erweitert
    altes app.css übertragen
    Namenskonventionen gepflegt
    page-appInhalt.js/css gelöscht, da neue Startseite "Gibt es heute Pommes"

--- public/App.js
+++ public/App.js
@@ -2,18 +2,53 @@ class App {
   constructor() {
 
 
-    this._pages = [
-      {
+    this._pages = [  // hier sind alle Klassen(Seiten) drin
+      { //Startseite - zeigt an ob es heute Pommes gibt oder nicht
+        name: "page-list", // Name zum Aufrufen der Seite
+        klass: PageGibtEsPommes, // Klasse, die die Seite bereitstellt
+        label: "Übersicht", // Bezeichnung der Seite
+        hidden: false, // Seite kann im Menü ausgewählt werden
+      },
+      { //Chat-Seite
         name: "page-chat", // Name zum Aufrufen der Seite
         klass: PageChat, // Klasse, die die Seite bereitstellt
         label: "Chat", // Bezeichnung der Seite
         hidden: false, // Seite kann im Menü ausgewählt werden
       },
-      {
-        name: "page-list", // Name zum Aufrufen der Seite
-        klass: PageGibtEsPommes, // Klasse, die die Seite bereitstellt
-        label: "Übersicht", // Bezeichnung der Seite
-        hidden: false, // Seite kann im Menü ausgewählt werden
+      {    //Seite zu GameOne
+          name: "page-GameOne",               // Name zum Aufrufen der Seite
+          klass: PageGameOne,                 // Klasse, die die Seite bereitstellt
+          label: "Lustiges Pommesspiel",      // Bezeichnung der Seite
+          hidden: false,                      // Seite kann im Menü ausgewählt werden
+      },
+      { 	 //Seite zu PommesStartseiteNew
+          name: "page-PommesStartseiteNew",
+          klass: PagePommesRezeptListe,
+          label: "Rezepte anzeigen",
+          hidden: false,                      // Seite kann im Menü ausgewählt werden
+      },
+      {    //Seite zu PommesRezepten Standardseite
+          name: "page-PommesRezeptListe",
+          klass: PagePommesRezeptListe,
+          label: "<a>Pommes Rezepte</a>",
+          hidden: true,                       // Seite kann im Menü ausgewählt werden
+      },
+      { 	 //Seite zu PommesRezeptenNew
+          name: "page-PommesRezeptNew",
+          klass: PagePommesRezeptEdit,
+          label: "Neues Rezept hinzufügen",
+          hidden: false,                      // Seite kann im Menü ausgewählt werden
+      },
+      {   //Seite zu PommesRezeptenEdit
+          name: "page-PommesRezeptEdit",
+          klass: PagePommesRezeptEdit,
+          hidden: true,                      // Seite kann im Menü NICHT ausgewählt werden
+      },
+      {   //Seite zu PommesRezeptenBild
+          name: "page-PommesRezeptBild",
+          klass: PagePommesRezeptBild,
+          label: "Bild aussuchen",
+          hidden: true,                   // Seite kann im Menü NICHT ausgewählt werden
       },
     ]
 
@@ -26,7 +61,7 @@ class App {
 
 
   _renderMenu() {
-      let ul = document.querySelector("#app-menu > ul");
+      let ul = document.querySelector("#app-menu > ul");    //Menüreiter anzeigen
       let template = document.getElementById("template-app-menu-li").innerHTML;
 
       this._pages.forEach(page => {
@@ -60,7 +95,7 @@ class App {
           return;
       }
 
-
+      // Aktuelle Seite ausblenden
       if(this._currentPageObject != null){
         if(this._currentPageObject.name === newPage.name){
           console.log("samePage");
