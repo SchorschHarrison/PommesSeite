2e89c18c4d408c565918644140a2d236b13206e5
Ann-Christin Gehring <ann-christin.gehring@gmx.de>
2018-10-29 18:04:39 +0100

    Rezept css überarbeitet, Kommentare für Rezepte in html und js
    css: Fehler behoben

--- public/page-pommesRezeptListe.js
+++ public/page-pommesRezeptListe.js
@@ -6,49 +6,53 @@ class PagePommesRezeptListe {
   constructor(app) {
     this._app = app;
     this._mainElement = document.getElementById("main-page-rezepte");
-  //  PagePommesRezeptListe._currentDialoge = null;
-//    this.currentDialoge_new = null;
+
+    //???
     if(PagePommesRezeptListe.btn_neuesRezept == null){
       PagePommesRezeptListe.btn_neuesRezept = document.getElementById("new_button");
       PagePommesRezeptListe.btn_neuesRezept.addEventListener("click", () => this.neuesRezept());
-      //console.log(PagePommesRezeptListe.btn_neuesRezept);
     }
+
+    //Aufruf der Hauptfunktion
     this._renderList();
   }
-
-
 
   show(){
     this._mainElement.classList.remove("hidden");
   }
 
-
   hide(){
     this._mainElement.classList.add("hidden");
+
+    //?
     if(PagePommesRezeptListe._currentDialoge != null ) {
       PagePommesRezeptListe._currentDialoge.hide();
       PagePommesRezeptListe._currentDialoge = null;
     }
   }
 
-
+//Hauptfunktion
   _renderList(){
     this._rezepte = this._app.getRezepte();
+    // Alte Einträge verwerfen
     let ol = document.getElementById("ol_rezepte");
     ol.innerHTML = "";
 
+    //Wenn Rezeptliste leer soll das template für die leere Liste aufgerfen werden
     if(this._rezepte.length < 1){
       let template = document.getElementById("template-page-list-empty").innerHTML;
       ol.innerHTML = template;
       return;
     }
 
+    // Datensätze einfügen
     let template = document.getElementById("template-rezept-data").innerHTML;
     let index = -1;
 
     this._rezepte.forEach(dataset => {
         index++;
-        //console.log(dataset);
+
+        // Neues Element auf Basis des Templates erzeugen
         let dummy = document.createElement("div");
         dummy.innerHTML = template;
         dummy.innerHTML = dummy.innerHTML.replace("$INDEX$", index);
@@ -56,28 +60,21 @@ class PagePommesRezeptListe {
         dummy.innerHTML = dummy.innerHTML.replace("$ZUTATEN$" , dataset.zutaten);
         dummy.innerHTML = dummy.innerHTML.replace("$ZUBEREITUNG$" , dataset.zubereitung);
         dummy.innerHTML = dummy.innerHTML.replace("$IMAGE$", dataset.image);
-        //// TODO: Add _addEventListeners
-
 
        let _addEventListeners = (index) => {
             // Event Listener für <div class="action edit"> registrieren
             let editButton = dummy.querySelector(".action.edit");
-            //editButton.addEventListener("click", () => this._app.showPage("page-edit", index));
-
             editButton.addEventListener("click", () => this._showEditDialog(index));
-
 
             // Event Listener für <div class="action delete"> registrieren
             let deleteButton = dummy.querySelector(".action.delete");
             deleteButton.addEventListener("click", () => this._askDelete(index));
         };
 
-
-
-
+        //Funktion _addEventListeners
         _addEventListeners(index);
 
-
+        // Sorgt dafür, dass die Einträge angezeigt werden
         let li = dummy.firstElementChild;
 
         if (li) {
@@ -86,11 +83,11 @@ class PagePommesRezeptListe {
         }
     }
 
-
   );
 
   }
 
+  //?
   _showEditDialog(index){
     this.hide();
     let editDialog = new PageRezeptEdit(this._app, "page_edit", index, this);
@@ -99,6 +96,7 @@ class PagePommesRezeptListe {
     editDialog.show();
   }
 
+  //?
   _showEditDialog2(index){
     this.hide();
     let editDialog = new PageRezeptEdit(this._app, "page_edit", index, this);
@@ -106,11 +104,12 @@ class PagePommesRezeptListe {
     console.log(editDialog);
     editDialog.show();
   }
-
+//Funktion zum löschen eines Rezepts
   _askDelete(index){
     let answer = confirm("Soll das ausgewählte Rezept wirklich gelöscht werden?");
     if (!answer) return;
 
+    //wenn nicht abbruch durch return, dann Rezept löschen
     this._app.loescheRezept(index);
 
     console.log("delete: " + index);
