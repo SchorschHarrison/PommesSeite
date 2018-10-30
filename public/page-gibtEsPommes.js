aef6c8b0ec506ad7292024b3592bf05437979ef7
SchorschHarrison <43842888+SchorschHarrison@users.noreply.github.com>
2018-10-30 19:08:15 +0100

    Übersicht angepasst

--- public/page-gibtEsPommes.js
+++ public/page-gibtEsPommes.js
@@ -1,14 +1,25 @@
+"use strict"
+
 class PageGibtEsPommes {
 
 
   constructor(app, name) {
     this._app = app;
     this.name = name;
-    this._mainElement = document.getElementById("main-page-gibtespommes");
-    if(PageGibtEsPommes.socket == '') PageGibtEsPommes.socket = io.connect("http://localhost:80");
 
-    //console.log(PageGibtEsPommes.socket.id);
-    PageGibtEsPommes.socket.on('gibt es heute pommes', (pommes) => this.pommesAnzeige(pommes));
+
+    this.answer = null;
+    this.answer_span = document.getElementById('answer');
+    this.answer_span.innerHTML = this.answer;
+    this._mainElement = document.getElementById("main-page-gibtespommes");
+    if(PageGibtEsPommes.socket == ''){
+      PageGibtEsPommes.socket = io.connect("http://localhost:80");
+      //console.log(PageGibtEsPommes.socket.id);
+      PageGibtEsPommes.socket.on('gibt es heute pommes', (pommes) => this.pommesAnzeige(pommes));
+    }
+
+
+
     PageGibtEsPommes.socket.emit('gibt es heute pommes');
   }
 
@@ -25,8 +36,12 @@ class PageGibtEsPommes {
   pommesAnzeige(pommes){
     if(pommes == 1){
       console.log("Pommes! :D");
+      this.answer_span.innerHTML = "ja";
+      console.log(this.answer);
     }else{
       console.log("Keine Pommes! :(");
+      this.answer_span.innerHTML = "nein";
+      console.log(this.answer);
     }
   }
 }
